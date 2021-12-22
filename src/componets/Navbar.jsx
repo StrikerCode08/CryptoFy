c5d9dc5721a2460ad8b14b7adf1f97d52276240f
Akash <akashbajpai227@gmail.com>
2021-12-22 18:58:28 +0530

    Intial Commit

---
+++ src/componets/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Button,Menu,Typography,Avatar} from 'antd' ;
+import {Link} from 'react-router-dom'
+import { HomeOutlined,MoneyCollectOutlined, BulbOutlined,FundOutlined,MenuOutlined } from '@ant-design/icons/lib/icons';
+
+const Navbar = () => {
+    return (
+        <div className='nav-container'>
+            <div className='logo-container'>
+            <Avatar src={icon} size='large' />
+            <Typography.Title level={2} className='logo'>
+            <Link to='/' > CryptoFy</Link>
+            </Typography.Title>
+            {/* <Button className='menu-contol-container'>
+
+            </Button> */}
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
