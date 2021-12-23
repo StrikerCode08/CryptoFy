d8cc860f173448940402fd89aa657c9c9b7e76f1
Akash <akashbajpai227@gmail.com>
2021-12-23 15:08:45 +0530

    Api Integration

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "7474976e25msh1a3da6874b78f65p149f9fjsn448334d606fc",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/coins";
+
+const createRequest = (url) => ({ baseUrl, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl: baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
